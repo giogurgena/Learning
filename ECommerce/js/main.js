cb037afdfe9e11184f6e59486a02a0ec44ea736c
Giorgi Gurgenashvili <giogurgena@Gmail.com>
2018-01-04 00:19:34 +0400

    doneeeeeeeee

--- ECommerce/js/main.js
+++ ECommerce/js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $('#myInput').on('keyup', function(){
+$(document).ready(function () {
+    $('#myInput').on('keyup', function () {
         var value = $(this).val().toLowerCase();
-        $('.dropdown-menu li').filter(function(){
-            $(this).toggle($(this).text().toLowerCase().indexOf(value)> -1)
+        $('.dropdown-menu li').filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
 
@@ -14,24 +14,35 @@ $(document).ready(function(){
 //     var $sup=$('sup');
 //     $sup.hide();
 //     $('.add-to-cart').on('click', function(){
-        
+
 //     $sup.show();
-        
+
 //     $sup.text(a++);
 //     });
 // })();
 
 
-(function(){
-    var a=1;
-    var $sup=$('sup');
-    
-    $('.add-to-cart').on('click', function(){
-        
-    $sup.text(a++);
+// (function(){
+//     var a=1;
+//     var $sup=$('sup');
+
+//     $('.add-to-cart').on('click', function(){
+
+//     $sup.text(a++);
+//     });
+// })();
+
+
+(function () {
+    $('.add-to-cart').on('click', function () {
+        var a = parseInt($('sup').text());
+        if (isNaN(a)) {
+            a = 0;
+        };
+        $('sup').text(++a);
+        console.log(a);
     });
 })();
-
 
 // (function(){
 //     $('.add-to-cart').on('click', function(){
