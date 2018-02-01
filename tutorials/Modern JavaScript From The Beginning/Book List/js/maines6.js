08fdb800fa2e7913aa1bc8ddbfa5612db655dd95
Giorgi Gurgenashvili <giogurgena@gmail.com>
2018-02-01 23:59:58 +0400

    book list ls

--- tutorials/Modern JavaScript From The Beginning/Book List/js/maines6.js
+++ tutorials/Modern JavaScript From The Beginning/Book List/js/maines6.js
@@ -56,6 +56,53 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+      const ui = new UI;
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function(book, index) {
+      if(book.isbn === isbn) {
+        books.splice(index, 1)
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+
 // Event Listeners for add book
 document.getElementById("book-form").addEventListener("submit", function(e) {
   // Get form values
@@ -77,6 +124,9 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
     // Add book to list
     ui.addBookToList(book);
 
+    // Add to LS
+    Store.addBook(book);
+
     // Show success
     ui.showAlert("Book Added!", "success");
 
@@ -94,6 +144,9 @@ document.getElementById("book-list").addEventListener("click", function(e) {
 
   // Delete Book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
   // Show message
   ui.showAlert("Book Removed!", "success");
