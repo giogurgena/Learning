50105f9ebd8a12a8d0ae5e43c1efa9ed79590a25
Giorgi Gurgenashvili <giogurgena@gmail.com>
2018-03-06 22:44:25 +0400

    c#

---
+++ C#/Store/Store/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Store.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PersonalNumber { get; set; }
+        public static DateTime Now { get; }
+        public double DateOfBirth { get; internal set; }
+    }
+}
---
+++ C#/Store/Store/Program.cs
@@ -0,0 +1,27 @@
+﻿using Store.Models;
+using System;
+using System.Globalization;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Gio";
+            person.LastName = "Gurgenashvili";
+            person.PersonalNumber = "45465464";
+            person.DateOfBirth = 07-12-1991;
+            Console.WriteLine(person.PersonalNumber);
+
+            Person client = new Person();
+            client.FirstName = "Nika";
+            person.LastName = "Kvijinadze";
+            person.PersonalNumber = "49465432";
+            person.DateOfBirth = 06-03-1992;
+            Console.WriteLine(client.PersonalNumber);
+        }
+
+    }
+}
