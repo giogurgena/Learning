b6dd61c0f36105b6fd87faed2c760fb808ccd1b1
newsgraphic213 <newsgraphic213@imedi.ge>
2018-11-08 11:40:14 +0400

    1:17:33 mosh react

--- tutorials/React/Programming with Mosh Learn React/mosh-react/src/components/counter.jsx
+++ tutorials/React/Programming with Mosh Learn React/mosh-react/src/components/counter.jsx
@@ -5,15 +5,26 @@ class Counter extends Component {
     count: 0
   };
 
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">
+        <span className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={ () => this.handleIncrement(product) } className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
