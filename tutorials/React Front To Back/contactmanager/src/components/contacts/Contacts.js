a4b53c88c5943c8148d43c7edf189524eac74054
Giorgi Gurgenashvili <giogurgena@gmail.com>
2018-11-19 15:49:29 +0400

    traversy react course contact manager app done

--- tutorials/React Front To Back/contactmanager/src/components/contacts/Contacts.js
+++ tutorials/React Front To Back/contactmanager/src/components/contacts/Contacts.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
-import {Consumer} from '../../context'
+import { Consumer } from '../../context';
 
 class Contacts extends Component {
-
   render() {
     return (
       <Consumer>
@@ -11,17 +10,17 @@ class Contacts extends Component {
           const { contacts } = value;
           return (
             <React.Fragment>
+              <h1 className="display-4 mb-4">
+                <span className="text-danger">Contact</span> List
+              </h1>
               {contacts.map(contact => (
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                />
+                <Contact key={contact.id} contact={contact} />
               ))}
             </React.Fragment>
-          )
+          );
         }}
       </Consumer>
-    )
+    );
   }
 }
 
