e8838693f7af4c313ac7bdd43d349c46ec52e55d
unknown <giogurgena@gmail.com>
2019-07-17 18:28:47 +0400

    mosh react 37 starting vidly project

---
+++ tutorials/Mosh/Mastering React/vidly/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="container">
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
