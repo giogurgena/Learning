07d984c98a87ccb4b1ba6de38b644999f442107e
unknown <giogurgena@gmail.com>
2019-07-25 17:21:15 +0400

    mosh react 64

--- tutorials/Mosh/Mastering React/vidly/src/components/movies.jsx
+++ tutorials/Mosh/Mastering React/vidly/src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Like from './common/like';
 import { getMovies } from '../services/fakeMovieService';
 
 class Movies extends Component {
@@ -9,6 +10,14 @@ class Movies extends Component {
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies: movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
 
   render() {
@@ -26,6 +35,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th />
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -35,6 +45,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
