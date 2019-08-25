f55a9bb2e94d9f659a19d872eab54c848630021c
Giorgi <giogurgena@gmail.com>
2019-08-25 17:28:05 +0400

    mosh react 57

--- tutorials/Mosh/Mastering React/counter-app/src/components/counter.jsx
+++ tutorials/Mosh/Mastering React/counter-app/src/components/counter.jsx
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    count: 0
-  };
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-      </React.Fragment>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? 'Zero' : count;
   }
 }
