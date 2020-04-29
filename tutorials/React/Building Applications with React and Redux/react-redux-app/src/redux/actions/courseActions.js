400f0edf829a54e22fac422d446af4dff9d1e5d6
unknown <giogurgena@gmail.com>
2020-04-29 19:24:56 +0400

    react redux Async Writes in Redux

--- tutorials/React/Building Applications with React and Redux/react-redux-app/src/redux/actions/courseActions.js
+++ tutorials/React/Building Applications with React and Redux/react-redux-app/src/redux/actions/courseActions.js
@@ -1,17 +1,24 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
 
-export function createCourse(course) {
-  return {
-    type: types.CREATE_COURSE,
-    course: course,
-  };
-}
-
 export function loadCoursesSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
     courses: courses,
+  };
+}
+
+export function createCoursesSuccess(course) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    courses: course,
+  };
+}
+
+export function updateCoursesSuccess(course) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    courses: course,
   };
 }
 
@@ -21,6 +28,21 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCoursesSuccess(savedCourse))
+          : dispatch(createCoursesSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
