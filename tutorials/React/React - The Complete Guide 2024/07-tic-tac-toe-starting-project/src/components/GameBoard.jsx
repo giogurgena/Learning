3d92b36c710a0f6681335c57bc3803dc054c42b5
giogurgena <giogurgena@gmail.com>
2024-03-05 09:00:46 +0400

    react tic tac toe

---
+++ tutorials/React/React - The Complete Guide 2024/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,37 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare }) {
+  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  //   function handleSelectSquare(rowIndex, colIndex) {
+  //     setGameBoard((previousGameBoard) => {
+  //       const updatedBoard = [
+  //         ...previousGameBoard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //       return updatedBoard;
+  //     });
+
+  //     onSelectSquare();
+  //   }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={onSelectSquare}>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
