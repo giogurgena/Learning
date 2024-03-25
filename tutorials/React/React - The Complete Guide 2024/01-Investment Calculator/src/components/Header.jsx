e7f2bb650b8d385488560df432eef2098b9afc06
giogurgena <giogurgena@gmail.com>
2024-03-25 12:41:09 +0400

    Investment Calculator done

---
+++ tutorials/React/React - The Complete Guide 2024/01-Investment Calculator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+export function Header() {
+  return (
+    <header id="header">
+      <img src={logo} alt="logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
