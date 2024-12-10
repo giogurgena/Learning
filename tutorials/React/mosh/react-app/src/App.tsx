f242cda83edf590c0dbc4b6404c49c65c8bf4b6d
giogurgena <giogurgena@gmail.com>
2024-12-10 18:48:11 +0400

    mosh zod

--- tutorials/React/mosh/react-app/src/App.tsx
+++ tutorials/React/mosh/react-app/src/App.tsx
@@ -1,6 +1,5 @@
 import ListGroup from "./components/ListGroup";
 import "./app.css";
-import React, { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import Like from "./components/Like";
@@ -8,6 +7,7 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/cart";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
+import React from "react";
 
 function App() {
   return (
