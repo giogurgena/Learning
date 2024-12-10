b375afc93be8c0340c28b154a82a98e4a7caaa5f
giogurgena <giogurgena@gmail.com>
2024-12-10 17:04:53 +0400

    mosh react starter

---
+++ tutorials/React/mosh/react-app/src/App.tsx
@@ -0,0 +1,20 @@
+import ListGroup from "./components/ListGroup";
+import "./app.css";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import Like from "./components/Like";
+import NavBar from "./components/NavBar";
+import Cart from "./components/cart";
+import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div>
+      <Form></Form>
+    </div>
+  );
+}
+
+export default App;
---
+++ tutorials/React/mosh/react-app/src/components/Alert.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible fade show">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-dismiss="alert"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+};
+
+export default Alert;
