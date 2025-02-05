4ab10a0cd53ec4c883bcbee9059fa8b21b7c63e8
giogurgena <giogurgena@gmail.com>
2025-02-05 16:50:08 +0400

    mosh mutation

--- tutorials/React/mosh/react-course-part2-starter-main/src/react-query/hooks/useTodos.ts
+++ tutorials/React/mosh/react-course-part2-starter-main/src/react-query/hooks/useTodos.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { CACHE_KEY_TODOS } from "../constants";
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   userId: number;
@@ -15,7 +16,7 @@ const useTodos = () => {
       .then((res) => res.data);
 
     return useQuery<Todo[], Error>({
-      queryKey: ["todos"],
+      queryKey: CACHE_KEY_TODOS,
       queryFn: fetchTodos,
       staleTime: 10*1000
     });
